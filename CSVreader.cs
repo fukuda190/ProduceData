27f054c9d1292af29c07c06fa2db074d867acd6e
fukuda190 <37056191+fukuda190@users.noreply.github.com>
2018-03-29 10:54:27 +0900

    クイズロードスクリプト更新1

--- CSVreader.cs
+++ CSVreader.cs
@@ -7,19 +7,14 @@ public class CSVreader : MonoBehaviour
 {
     public TextAsset CSVfile;
     public List<string[]> csvDatas = new List<string[]>();
-    public Text Quiz;
-    public Text Select1, Select2, Select3, Select4;
+    public Text Quiz,Select1, Select2, Select3, Select4;
     string Answer;
-    public Text IncorrectanswerText;
-    public Text CorrectText;
+    public Text CorrectText, IncorrectanswerText;
     private AudioSource[] sources;
     public CanvasGroup Select1off, Select2off, Select3off, Select4off;
 
-
-
     // Use this for initialization
-    void Start()
-    {
+    void Start(){
         sources = gameObject.GetComponents<AudioSource>();
         Select1off.alpha = 1;
         Select2off.alpha = 1;
@@ -27,18 +22,11 @@ public class CSVreader : MonoBehaviour
         Select4off.alpha = 1;
         StartCoroutine("Read");
     }
-    void Update()
-    {
 
-
-    }
-
-    // Update is called once per frame
-    public void QuizLoad()
-    {
+    //CSVfileを読み込み、リストにカンマ区切りで格納
+    public void QuizLoad(){
         string[] lines = CSVfile.text.Replace("\r\n", "\n").Split("\n"[0]);
-        foreach (string line in lines)
-        {
+        foreach (string line in lines){
             if (line == "") { continue; }
             csvDatas.Add(line.Split(','));
         }
@@ -51,69 +39,65 @@ public class CSVreader : MonoBehaviour
         Select3.text = csvDatas[RandomNum][4];
         Select4.text = csvDatas[RandomNum][5];
     }
-    IEnumerator Read()
-    {
-        yield return new WaitForSeconds(0);
+    IEnumerator Read(){
+        yield return new WaitForSeconds(0.5f);
         QuizLoad();
     }
-    public void Display1()
-    {
-        if (Answer == Select1.text)
-        {
+    public void Display1(){
+        if (Answer == Select1.text){
             CorrectText.text = "正解";
             sources[0].Play();
             GameObject.Find("TimeObject").GetComponent<TimeCount>().ButtonDisplay();
+            GameObject.Find("TimeObject").GetComponent<TimeCount>().StopTime();
         }
-        else if (Answer != Select1.text)
-        {
+        else if (Answer != Select1.text){
             IncorrectanswerText.text = "不正解";
             sources[1].Play();
             GameObject.Find("TimeObject").GetComponent<TimeCount>().ButtonDisplay();
+            GameObject.Find("TimeObject").GetComponent<TimeCount>().StopTime();
         }
     }
-    public void Display2()
-    {
-        if (Answer == Select2.text)
-        {
+    public void Display2(){
+        if (Answer == Select2.text){
             CorrectText.text = "正解";
             sources[0].Play();
             GameObject.Find("TimeObject").GetComponent<TimeCount>().ButtonDisplay();
+            GameObject.Find("TimeObject").GetComponent<TimeCount>().StopTime();
         }
-        else if (Answer != Select2.text)
-        {
+        else if (Answer != Select2.text){
             IncorrectanswerText.text = "不正解";
             sources[1].Play();
             GameObject.Find("TimeObject").GetComponent<TimeCount>().ButtonDisplay();
+            GameObject.Find("TimeObject").GetComponent<TimeCount>().StopTime();
         }
     }
-    public void Display3()
-    {
-        if (Answer == Select3.text)
-        {
+    public void Display3(){
+        if (Answer == Select3.text){
             CorrectText.text = "正解";
             sources[0].Play();
             GameObject.Find("TimeObject").GetComponent<TimeCount>().ButtonDisplay();
+            GameObject.Find("TimeObject").GetComponent<TimeCount>().StopTime();
         }
-        else if (Answer != Select3.text)
-        {
+        else if (Answer != Select3.text){
             IncorrectanswerText.text = "不正解";
             sources[1].Play();
             GameObject.Find("TimeObject").GetComponent<TimeCount>().ButtonDisplay();
+            GameObject.Find("TimeObject").GetComponent<TimeCount>().StopTime();
         }
     }
-    public void Display4()
-    {
-        if (Answer == Select4.text)
-        {
+    public void Display4(){
+        if (Answer == Select4.text){     //選択肢4の判定
             CorrectText.text = "正解";
             sources[0].Play();
             GameObject.Find("TimeObject").GetComponent<TimeCount>().ButtonDisplay();
+            GameObject.Find("TimeObject").GetComponent<TimeCount>().StopTime();
         }
         else if (Answer != Select4.text)
         {
             IncorrectanswerText.text = "不正解";
             sources[1].Play();
             GameObject.Find("TimeObject").GetComponent<TimeCount>().ButtonDisplay();
+            GameObject.Find("TimeObject").GetComponent<TimeCount>().StopTime();
         }
     }
     public void DisPlayoff()
@@ -128,6 +112,7 @@ public class CSVreader : MonoBehaviour
         Select3off.alpha = 0;
         Select4off.alpha = 0;
     }
+    
 }
 
 
